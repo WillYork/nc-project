14a2cd0c10ef0368cadfa36b8667319c353fa1d8
William York <williamyork@Williams-MacBook-Pro.local>
2019-09-16 10:48:21 +0100

    remove gitignore files

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const ENV = process.env.NODE_ENV || 'development';
-const testData = require('./test-data');
-const devData = require('./dev-data');
+const testData = require('./data/test-data');
+const devData = require('./data/development-data');
 
 const data = {
   development: devData,
