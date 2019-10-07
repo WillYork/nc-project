8c8b3a07c94773654bc4b25f9e39ecf37ebe73f3
William York <williamyork@Williams-MacBook-Pro.local>
2019-10-07 11:26:31 +0100

    added cors

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const {
   customErrorHandler,
   send405Error
 } = require("./error-handlers");
+const cors = require('cors')
+app.use(cors())
 app.use(express.json());
 
 app.use("/api", apiRouter);
